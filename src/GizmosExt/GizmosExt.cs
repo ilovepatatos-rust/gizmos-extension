3064db70c8a15a21ae06f6906b1648ab663e3d4e
Ilovepatatos <gabriel@khome.org>
2025-01-20 07:38:07 -0500

    Renaming

--- src/GizmosExt/GizmosExt.cs
+++ src/GizmosExt/GizmosExt.cs
@@ -7,11 +7,11 @@ namespace Oxide.Ext.GizmosExt
     [UsedImplicitly]
     public class GizmosExt : Extension
     {
-        private static readonly VersionNumber s_ExtensionVersion = new(1, 0, 0);
+        private static readonly VersionNumber s_extensionVersion = new(1, 0, 0);
 
         public override string Name => "GizmosExt";
         public override string Author => "Ilovepatatos";
-        public override VersionNumber Version => s_ExtensionVersion;
+        public override VersionNumber Version => s_extensionVersion;
 
         public override bool SupportsReloading => true;
 
